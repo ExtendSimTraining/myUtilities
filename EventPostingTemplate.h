d64cc141ee449ade824f5485c024cedbd3810fef
Robin Clark <training01@qmtgroup.com>
2015-03-10 01:53:07 -0400

    Updated with Library Version.

--- EventPostingTemplate.h
+++ EventPostingTemplate.h
@@ -1,5 +1,7 @@
 **************************
-** Block: myUtiliteis.lib \ EventPostingTemplate
+** Block: EventPostingTemplate
+** Lib: myUtiliteis.lib 
+** Lib: Version: 9.0.0
 **************************
 
 **************************
